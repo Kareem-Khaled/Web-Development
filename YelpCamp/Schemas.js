80072da27fb38ea834f6a321127b65e836d21bdc
Kareem-Khaled <kareemkhaled143@gmail.com>
2021-03-07 18:44:57 +0200

    add

---
+++ YelpCamp/Schemas.js
@@ -0,0 +1,18 @@
+const joi = require('joi');
+
+module.exports.campgroundSchema = joi.object({
+    campground: joi.object({
+        title: joi.string().required(),
+        image: joi.string(),
+        price: joi.string().required().min(0),
+        description: joi.string().required(),
+        location: joi.string().required()
+    }).required()
+})
+
+module.exports.reviewSchema = joi.object({
+    review:joi.object({
+        body :joi.string().required(),
+        rating: joi.number().required()
+    }).required()
+})
